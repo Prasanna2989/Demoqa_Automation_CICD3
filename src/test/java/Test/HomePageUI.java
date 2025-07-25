41187ee7b5e94474cd4ec802d4661bbd4d8ad081
Prasanna2989 <pmgunawardana@gmail.com>
2025-07-26 08:50:10 +1000

    Initial commit

---
+++ src/test/java/Test/HomePageUI.java
@@ -0,0 +1,35 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HomePageUI {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test(priority = 1)
+    public void vrfyHomePageUI() {
+        driver.get("https://demoqa.com");
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        Assert.assertTrue(title.contains("ToolsQA"), "Title doesn't match!");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
